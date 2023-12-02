b6557de525af5cfc70ae89b0610021f4b082e4d7
Elena Pakulina <el.pakulina@yandex.ru>
2023-12-02 22:53:50 +0500

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,5 +9,13 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен ages: " + ages.length);
         System.out.println("Размер массива равен surnames: " + surnames.length);
         System.out.println("Размер массива равен prices: " + prices.length);
+
+        String[] names = new String[4];
+        for (int i = 0; i < names.length; i++) {
+            names[i] = "Random" + i  + " Name" + 1;
+        }
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
